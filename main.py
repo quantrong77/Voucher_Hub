2049061b3a2f6be6f02bac0cba4eb4f1d44505cb
Hoang Trong <quantrong77@gmail.com>
2023-05-20 19:48:17 +0700

    add Procfile file

--- main.py
+++ main.py
@@ -135,8 +135,3 @@ async def get_open_api_endpoint():
         description="This is the auto-generated OpenAPI schema for the API",
         routes=app.routes
     )
-
-   
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
-
